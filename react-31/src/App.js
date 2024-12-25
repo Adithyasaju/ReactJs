f169ce5eee82ebefa574a2abcb480f5035752393
Adithyasaju <adithyasajuo7oaaa2gmail.com>
2024-12-25 20:26:35 +0530

    Learn

--- react-31/src/App.js
+++ react-31/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Message from './Message/Message'
+import { Provider } from 'react-redux'
+import { store } from './Redux/store'
 const App=()=>{
     return(
         <div>
+            <Provider store={store}>
             <h2>App Component</h2>
             <Message/>
+
+            </Provider>
         </div>
     )
 }
